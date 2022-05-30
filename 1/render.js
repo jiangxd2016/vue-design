350c9657eac59f09a2f4249fad4c11a12a93a8d0
jiangxd <jiangxd2016@gmail.com>
2022-05-30 21:07:36 +0800

    chore: Chapter I

---
+++ 1/render.js
@@ -0,0 +1,38 @@
+const obj = {
+  tag:"div",
+  children:[
+    {
+      tag:"h1",
+      children:[
+        "Hello",
+        {
+          tag:"p",
+          children:"World"
+        }
+      ]
+    },
+  ]
+}
+
+
+function render(obj,root){
+  const el = document.createElement(obj.tag);
+  if(typeof obj.children === "string"){
+    el.innerText = obj.children;
+  }else if(Array.isArray(obj.children)){
+    obj.children.forEach(child=>{
+      if(typeof child === "string"){
+        el.innerText += child;
+      }
+      else{
+        render(child,el);
+      }
+    });
+  }
+  
+
+  root.appendChild(el)
+}
+
+
+render(obj,document.body);
