2c2c52fae733c9ecc6cf3f3ebd2be509669f8f09
jiangxd <jiangxd2016@gmail.com>
2022-06-08 22:52:22 +0800

    3.2

---
+++ 3.2/render.js
@@ -0,0 +1,28 @@
+const vnode = {
+  tag: 'div',
+  props: {
+    onClick: () => alert("click")
+  },
+  children: "Hello World"
+}
+
+
+function render(vNode, container) {
+  const el = document.createElement(vNode.tag);
+
+  for (const key in vNode.props) {
+    if (/^on/.test(key)) {
+      const event = key.slice(2).toLowerCase();
+      el.addEventListener(event, vNode.props[key]);
+    }
+  }
+  if (Array.isArray(vNode.children)) {
+    vNode.children.forEach(child => render(child,el));
+  } else {
+    el.appendChild(document.createTextNode(vNode.children));
+  }
+
+  container.appendChild(el);
+}
+
+render(vnode, document.body);
